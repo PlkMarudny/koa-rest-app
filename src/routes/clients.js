16710523e1a2eeaa933d5b4b56faa415668bb537
Tomasz Plonka <tomasz.plonka@basharq.com>
2021-01-28 16:40:39 +0400

    fix: use logger.looger in 'clients' route

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -1,5 +1,6 @@
 import Router from "@koa/router";
 import { socket } from "../server";
+import logger from '../logger';
 
 
 const router = new Router();
@@ -8,7 +9,7 @@ router.get('/clients', async ctx => {
     let data = [];
     socket.forEach(function (spark) {
         data.push({ user: spark.user });
-        ctx.logger.debug(data);
+        logger.logger.debug(data);
     });
     ctx.body = data;
 });
